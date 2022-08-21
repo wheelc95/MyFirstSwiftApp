d48493d30c9298ebca848c1fd6950db87cb428a1
wheelc95 <wheel.c.code@gmail.com>
2022-08-21 15:47:56 -0400

    Initial Commit

---
+++ Shared/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  Shared
+//
+//  Created by Charah Wheeler on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
